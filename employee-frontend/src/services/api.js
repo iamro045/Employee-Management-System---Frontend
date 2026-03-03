e95eacff1a28b1ddfe64f9e73e0d8fb5c8f082f7
Rohit Pravin Shinde <rohitshinde7922@gmail.com>
2026-03-03 21:24:20 +0530

    Update API base URL to use environment variable

--- employee-frontend/src/services/api.js
+++ employee-frontend/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
+export default API;
 // Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
