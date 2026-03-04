b0d40bdbe981b7a9c30cf1f2add27c5f92dcbc85
Rohit Pravin Shinde <rohitshinde7922@gmail.com>
2026-03-04 20:02:55 +0530

    Update Login.jsx

--- employee-frontend/src/pages/Login.jsx
+++ employee-frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await API.post("/auth/login", {
+      const res = await API.post("/api/auth/login", {
         email,
         password,
       });
